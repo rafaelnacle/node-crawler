e2bd351f0a4834940be186f3555980f8d418bed5
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-03-26 19:30:59 -0300

    sortPages and testcases

---
+++ report.js
@@ -0,0 +1,13 @@
+function sortPages(pages) {
+  const pagesArr = Object.entries(pages)
+  pagesArr.sort((a, b) => {
+    aHits = a[1]
+    bHits = b[1]
+    return b[1] - a[1]
+  })
+  return pagesArr
+}
+
+module.exports = {
+  sortPages
+}
